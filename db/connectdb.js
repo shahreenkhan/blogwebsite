2174e12b1da8346d4b395fce7221ccd4a74ac6c3
shahreenkhan <shareenkhan04@gmail.com>
2023-07-19 10:59:59 +0530

    new

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectdb = ()=>{
-    con="mongodb+srv://shareenkhan:ashreen1997@cluster0.veo6aka.mongodb.net/blogwebsite?retryWrites=true&w=majority"
+    con="mongodb+srv://shahreen:Ashreen1997@cluster0.veo6aka.mongodb.net/blogwebsite?retryWrites=true&w=majority"
 
     //return mongoose.connect('mongodb://localhost:27017/blog_project') //mongodb host name
     return mongoose.connect(con)
